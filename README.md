830e3f771d34a1d22fcfaad7c451456c2a8c662b
Naoto Igarashi <spc0314@gmail.com>
2019-07-24 17:30:23 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# golang-test
+# HTTP Access via Socks5 Proxy
+
+You can specify a proxy server for each HTTP request. In this sample, http://inet-ip.info/ip endpoint will return different access source IP address for each proxy.
+
+# Getting Started
+
+## Create Proxy Servers
+
+Create Linux server on different sites. (e.g. Azure, AWS, Other region...)
+
+## Setting for All Proxy Severs 
+
+```shell
+sudo vi /etc/ssh/sshd_config
+
+# Enable "AllowTcpForwarding yes" setting
+
+sudo systemctl restart sshd
+```
+
+## Proxy Client setting
+
+ssh to each ssh server as Sock5 Proxy
+
+```sh
+ssh <username>@<proxy server ip or url> -N -f -D <portnumber>
+```
+
+```sh
+ssh ec2-user@ec2-18-182-66-111.ap-northeast-1.compute.amazonaws.com -N -f -D 10001
+```
+
+## Program Setting
+
+Set the proxy port in config.json
+
+```json
+{
+	"url": "http://inet-ip.info/ip",
+	"proxyPort": "10001"
+}
+```
+
+## Run
+
+```sh
+go run main.go
+```
+
+# Notes
+When execute from shell
+```sh
+curl --proxy socks5://localhost:8080 http://inet-ip.info/ip
+```
--- main.go
+++ main.go
@@ -4,20 +4,46 @@ import (
   "log"
   "fmt"
   "net/http"
+  "golang.org/x/net/proxy"
   "io/ioutil"
+  "encoding/json"
 )
 
+// Define type for config.json
+type Config struct {
+  Url     string    `json:"url"`
+  ProxyPorts []string `json:"proxyPorts"`
+}
 
 func main() {
-  url := "http://inet-ip.info/ip"
-
-  resp, err := http.Get(url)
+  // Load config
+  raw, err := ioutil.ReadFile("./config.json")
   if err != nil {
-    log.Fatal(err)
+      fmt.Println(err.Error())
+      //os.Exit(1)
   }
+  var c Config
+  json.Unmarshal(raw, &c)
 
-  defer resp.Body.Close()
+  client := http.DefaultClient
 
-  byteArray, _ := ioutil.ReadAll(resp.Body)
-  fmt.Println(string(byteArray)) // htmlをstringで取得 
+  for i, proxyPort := range c.ProxyPorts {
+    if proxyPort != "" {
+      p, _ := proxy.SOCKS5("tcp", "127.0.0.1:"+proxyPort, nil, proxy.Direct)
+      client.Transport = &http.Transport{
+          Dial: p.Dial,
+      }  
+    }
+  
+    resp, err := client.Get(c.Url)
+  
+    if err != nil {
+      log.Fatal(err)
+    }
+  
+    defer resp.Body.Close()
+  
+    byteArray, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println(i+1, ": You are accessing from", string(byteArray), "via proxy port",  proxyPort)
+  }
 }
